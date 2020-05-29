2a30fecb1d0868c58bc21244afbba9aa496c2078
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-05-29 23:11:58 +0100

    move functions out of line

--- common.hpp
+++ common.hpp
@@ -88,93 +88,10 @@ struct slot_bitmap
     return detail::nthbitset64(d, index_to_subindex(i));
   }
 
-  bool claim_slot(size_t i)
-  {
-    assert(i < N);
-    size_t w = index_to_element(i);
-    uint64_t subindex = index_to_subindex(i);
+  // probably want a specialisation where the slot is required to be empty initially
+  bool claim_slot(size_t i);
 
-    uint64_t d = load_relaxed(w);
-    uint64_t* addr = &data[w];
-
-    for (;;)
-      {
-        // if the bit was already set then we've lost the race
-
-        // can either check the bit is zero, or unconditionally set it and check
-        // if this changed the value
-        uint64_t proposed = detail::setnthbit64(d, subindex);
-        if (proposed == d)
-          {
-            return false;
-          }
-
-        // bool __atomic_compare_exchange(size_t size, void *ptr, void
-        // *expected, void *desired, int success_order, int failure_order)
-        // not sure about the memory orders
-        // need to use an intrinsic which understand scope
-
-        // opencl wants the array marked atomic, which might be fair enough
-        //(void)__opencl_atomic_compare_exchange_strong(
-        // (_Atomic volatile T *)address, &compare, val, __ATOMIC_SEQ_CST,
-        // __ATOMIC_RELAXED, __OPENCL_MEMORY_SCOPE_ALL_SVM_DEVICES);
-
-        uint64_t compare = d;
-        bool r = __atomic_compare_exchange(addr, &compare, &proposed, false,
-                                           __ATOMIC_SEQ_CST, __ATOMIC_RELAXED);
-
-        if (r)
-          {
-            // success, it's all ours
-            return true;
-          }
-
-        // cas failed. reasons:
-        // we lost the slot
-        // another slot in the same word changed
-        // spurious
-        // in any case, go around again
-
-        d = proposed;  // docs not totally clear, but an updated copy of the
-                       // word should be in one of the passed parameters. Might
-                       // be in compare
-      }
-  }
-
-  void release_slot(size_t i)
-  {
-    // programming error if the slot is not set on entry
-    assert(i < N);
-    size_t w = index_to_element(i);
-    uint64_t subindex = index_to_subindex(i);
-
-    uint64_t d = load_relaxed(w);
-    uint64_t* addr = &data[w];
-
-    for (;;)
-      {
-        uint64_t proposed = detail::clearnthbit64(d, subindex);
-        // error if set on entry
-        // cas should not return false if the write succeeded
-        assert(proposed != d);
-
-        uint64_t compare = d;
-        bool r = __atomic_compare_exchange(addr, &compare, &proposed, false,
-                                           __ATOMIC_SEQ_CST, __ATOMIC_RELAXED);
-
-        if (r)
-          {
-            // success, it's all ours
-            return;
-          }
-
-        // cas failed. reasons:
-        // another slot in the same word changed
-        // spurious
-
-        d = proposed;
-      }
-  }
+  void release_slot(size_t i);
 
  private:
   static uint64_t index_to_element(uint64_t x)
@@ -196,6 +113,96 @@ struct slot_bitmap
 
   alignas(64) uint64_t data[N / 64] = {0};
 };
+
+template <size_t N, size_t scope>
+bool slot_bitmap<N, scope>::claim_slot(size_t i)
+{
+  assert(i < N);
+  size_t w = index_to_element(i);
+  uint64_t subindex = index_to_subindex(i);
+
+  uint64_t d = load_relaxed(w);
+  uint64_t* addr = &data[w];
+
+  for (;;)
+    {
+      // if the bit was already set then we've lost the race
+
+      // can either check the bit is zero, or unconditionally set it and check
+      // if this changed the value
+      uint64_t proposed = detail::setnthbit64(d, subindex);
+      if (proposed == d)
+        {
+          return false;
+        }
+
+      // bool __atomic_compare_exchange(size_t size, void *ptr, void
+      // *expected, void *desired, int success_order, int failure_order)
+      // not sure about the memory orders
+      // need to use an intrinsic which understand scope
+
+      // opencl wants the array marked atomic, which might be fair enough
+      //(void)__opencl_atomic_compare_exchange_strong(
+      // (_Atomic volatile T *)address, &compare, val, __ATOMIC_SEQ_CST,
+      // __ATOMIC_RELAXED, __OPENCL_MEMORY_SCOPE_ALL_SVM_DEVICES);
+
+      uint64_t compare = d;
+      bool r = __atomic_compare_exchange(addr, &compare, &proposed, false,
+                                         __ATOMIC_SEQ_CST, __ATOMIC_RELAXED);
+
+      if (r)
+        {
+          // success, it's all ours
+          return true;
+        }
+
+      // cas failed. reasons:
+      // we lost the slot
+      // another slot in the same word changed
+      // spurious
+      // in any case, go around again
+
+      d = proposed;  // docs not totally clear, but an updated copy of the
+                     // word should be in one of the passed parameters. Might
+                     // be in compare
+    }
+}
+
+template <size_t N, size_t scope>
+void slot_bitmap<N, scope>::release_slot(size_t i)
+{
+  // programming error if the slot is not set on entry
+  assert(i < N);
+  size_t w = index_to_element(i);
+  uint64_t subindex = index_to_subindex(i);
+
+  uint64_t d = load_relaxed(w);
+  uint64_t* addr = &data[w];
+
+  for (;;)
+    {
+      uint64_t proposed = detail::clearnthbit64(d, subindex);
+      // error if set on entry
+      // cas should not return false if the write succeeded
+      assert(proposed != d);
+
+      uint64_t compare = d;
+      bool r = __atomic_compare_exchange(addr, &compare, &proposed, false,
+                                         __ATOMIC_SEQ_CST, __ATOMIC_RELAXED);
+
+      if (r)
+        {
+          // success, it's all ours
+          return;
+        }
+
+      // cas failed. reasons:
+      // another slot in the same word changed
+      // spurious
+
+      d = proposed;
+    }
+}
 
 }  // namespace hostrpc
 
