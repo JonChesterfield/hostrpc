cc2710245b819613b042410ba7458366e5288801
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-11-04 18:34:37 +0000

    drop no longer used dispatch_id

--- hostrpc/clean.sh
+++ hostrpc/clean.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cp dispatch_id.ll tmp
-for dir in "." loader codegen ".."; do
+for dir in "." loader detail codegen ".."; do
     rm -rf $dir/*.ll $dir/*.bc $dir/*.s $dir/*.obj $dir/*.exe $dir/*.so $dir/*device.o $dir/a.out
 done
-mv tmp dispatch_id.ll
 
 exit 0
