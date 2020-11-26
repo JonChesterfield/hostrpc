fa7a195cc136f41359c87c9cdcb5f8da0f006131
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-11-26 01:57:05 +0000

    better printing when calling weak symbols

--- hostrpc/allocator_openmp.cpp
+++ hostrpc/allocator_openmp.cpp
@@ -13,18 +13,18 @@ namespace hsa_impl
 {
 __attribute__((weak)) HOSTRPC_ANNOTATE void *allocate_fine_grain(size_t)
 {
-  fprintf(stderr, "Called weak symbol\n");
+  fprintf(stderr, "Called weak symbol: %s\n", __func__);
   return nullptr;
 }
 __attribute__((weak)) HOSTRPC_ANNOTATE int deallocate(void *)
 {
-  fprintf(stderr, "Called weak symbol\n");
+  fprintf(stderr, "Called weak symbol: %s\n", __func__);
   return 1;
 }
 
 __attribute__((weak)) HOSTRPC_ANNOTATE int memsetzero_gpu(void *, size_t)
 {
-  fprintf(stderr, "Called weak symbol\n");
+  fprintf(stderr, "Called weak symbol: %s\n", __func__);
   return 1;
 }
 
@@ -34,18 +34,18 @@ namespace cuda_impl
 {
 __attribute__((weak)) HOSTRPC_ANNOTATE void *allocate_shared(size_t)
 {
-  fprintf(stderr, "Called weak symbol\n");
+  fprintf(stderr, "Called weak symbol: %s\n", __func__);
 
   return nullptr;
 }
 __attribute__((weak)) HOSTRPC_ANNOTATE int deallocate_shared(void *)
 {
-  fprintf(stderr, "Called weak symbol\n");
+  fprintf(stderr, "Called weak symbol: %s\n", __func__);
   return 1;
 }
 __attribute__((weak)) HOSTRPC_ANNOTATE int memsetzero_gpu(void *, size_t)
 {
-  fprintf(stderr, "Called weak symbol\n");
+  fprintf(stderr, "Called weak symbol: %s\n", __func__);
   return 1;
 }
 }  // namespace cuda_impl
