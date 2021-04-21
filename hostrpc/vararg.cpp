dc899b805b26ced8ea2c2a12f3b57a076ed4ca95
Jon Chesterfield <jonathanchesterfield@gmail.com>
2021-04-21 21:22:26 +0100

    trie approach, sadly, is just too slow. Going to have to parse the format string more carefully

--- hostrpc/vararg.cpp
+++ hostrpc/vararg.cpp
@@ -1,10 +1,13 @@
+#include <array>
 #include <cassert>
 #include <cstdint>
 #include <cstdio>
+#include <string>
 #include <tuple>
+#include <utility>
 #include <vector>
 
-enum func_type : uint64_t
+enum func_type : uint8_t
 {
   func_print_nop = 0,
   func_print_uuu = 1,
@@ -18,9 +21,7 @@ enum func_type : uint64_t
 
   func_piecewise_pass_element_scalar = 8,
 
-  func_piecewise_pass_element_int32,
   func_piecewise_pass_element_uint32,
-  func_piecewise_pass_element_int64,
   func_piecewise_pass_element_uint64,
   func_piecewise_pass_element_double,
   func_piecewise_pass_element_void,
@@ -33,20 +34,22 @@ struct field
 {
   field() = default;
 
-  field(int32_t x) : tag(func_piecewise_pass_element_int32)
+  // 32 bit integers
+  field(uint32_t x) : tag(func_piecewise_pass_element_uint32)
+  {
+    u64_ = (uint64_t)x;
+  }
+  field(int32_t x) : tag(func_piecewise_pass_element_uint32)
   {
     int64_t tmp = x;
     __builtin_memcpy(&u64_, &tmp, 8);
   }
 
-  field(int64_t x) : tag(func_piecewise_pass_element_int64)
+  field(uint64_t x) : tag(func_piecewise_pass_element_uint64) { u64_ = x; }
+  field(int64_t x) : tag(func_piecewise_pass_element_uint64)
   {
     __builtin_memcpy(&u64_, &x, 8);
   }
-
-  field(uint32_t x) : tag(func_piecewise_pass_element_uint32) { u64_ = x; }
-
-  field(uint64_t x) : tag(func_piecewise_pass_element_uint64) { u64_ = x; }
 
   field(double x) : tag(func_piecewise_pass_element_double) { dbl_ = x; }
 
@@ -79,51 +82,108 @@ struct dump;
 template <>
 struct dump<int32_t>
 {
-  dump() { printf(" int32_t"); }
+  static const char* str() { return " int32_t"; }
+  dump() { printf("%s", str()); }
 };
 
 template <>
 struct dump<uint32_t>
 {
-  dump() { printf(" uint32_t"); }
+  static const char* str() { return " uint32_t"; }
+  dump() { printf("%s", str()); }
 };
 
 template <>
 struct dump<int64_t>
 {
-  dump() { printf(" int64_t"); }
+  static const char* str() { return " int64_t"; }
+  dump() { printf("%s", str()); }
 };
 
 template <>
 struct dump<uint64_t>
 {
-  dump() { printf(" uint64_t"); }
+  static const char* str() { return " uint64_t"; }
+  dump() { printf("%s", str()); }
 };
 
 template <>
 struct dump<double>
 {
-  dump() { printf(" double"); }
+  static const char* str() { return " double"; }
+  dump() { printf("%s", str()); }
 };
 
 template <>
 struct dump<const char*>
 {
-  dump() { printf(" const-char-*"); }
+  static const char* str() { return " const-char-*"; }
+  dump() { printf("%s", str()); }
 };
 
 template <>
 struct dump<void*>
 {
-  dump() { printf(" void*"); }
+  static const char* str() { return " void*"; }
+  dump() { printf("%s", str()); }
 };
+
+// c++14, courtesy of dietmar
+template <typename T, std::size_t N, std::size_t... I>
+constexpr std::array<T, N + 1> append_aux(std::array<T, N> a, T t,
+                                          std::index_sequence<I...>)
+{
+  return std::array<T, N + 1>{a[I]..., t};
+}
+template <typename T, std::size_t N>
+constexpr std::array<T, N + 1> append(std::array<T, N> a, T t)
+{
+  return append_aux(a, t, std::make_index_sequence<N>());
+}
+
+namespace
+{
+template <size_t ToDerive, typename asTup>
+__attribute__((noinline)) int baseprint(const std::vector<field>& args)
+{
+  printf("Narg %zu (", ToDerive);
+
+  std::string tmp;
+
+  if constexpr (ToDerive > 0)
+    {
+      using Ty = dump<typename std::tuple_element<0, asTup>::type>;
+      tmp += std::string(Ty::str());
+    }
+  if constexpr (ToDerive > 1)
+    {
+      using Ty = dump<typename std::tuple_element<1, asTup>::type>;
+      tmp += std::string(Ty::str());
+    }
+  if constexpr (ToDerive > 2)
+    {
+      using Ty = dump<typename std::tuple_element<2, asTup>::type>;
+      tmp += std::string(Ty::str());
+    }
+  if constexpr (ToDerive > 3)
+    {
+      using Ty = dump<typename std::tuple_element<3, asTup>::type>;
+      tmp += std::string(Ty::str());
+    }
+  if constexpr (ToDerive > 4)
+    {
+      using Ty = dump<typename std::tuple_element<4, asTup>::type>;
+      tmp += std::string(Ty::str());
+    }
+  return printf("%s)\n", tmp.c_str());
+}
 
 template <size_t ToDerive, size_t Derived, typename... Ts>
 struct interpT
 {
   static_assert(Derived < ToDerive, "");
 
-  void operator()(const std::vector<field>& args)
+  static void call(const std::vector<field>& args)
   {
     const field& f = args[Derived];
 
@@ -137,23 +197,25 @@ struct interpT
         // May need to specialise on long double (though amd64 thinks that
         // is a 80 bit value, so interop with amdgpu may be poor) or on
         // 16 bytes types
-        case func_piecewise_pass_element_int32:
-          using T = interpT<ToDerive, NextDerived, Ts..., int32_t>;
-          return T{}(args);
+
+        // x64 promotes 32 to 64 bit integer, haven't checked other arch so keep
+        // the distinction here
+        
         case func_piecewise_pass_element_uint32:
-          return interpT<ToDerive, NextDerived, Ts..., uint32_t>{}(args);
-        case func_piecewise_pass_element_int64:
-          return interpT<ToDerive, NextDerived, Ts..., int64_t>{}(args);
+          return interpT<ToDerive, NextDerived, Ts..., uint32_t>::call(args);
+
+          // these are all 64 bit integers, assuming sizeof(void)
         case func_piecewise_pass_element_uint64:
-          return interpT<ToDerive, NextDerived, Ts..., uint64_t>{}(args);
-        case func_piecewise_pass_element_double:
-          return interpT<ToDerive, NextDerived, Ts..., double>{}(args);
-
         case func_piecewise_pass_element_void:
-          return interpT<ToDerive, NextDerived, Ts..., void*>{}(args);
-
         case func_piecewise_pass_element_cstr:
-          return interpT<ToDerive, NextDerived, Ts..., const char*>{}(args);
+          return interpT<ToDerive, NextDerived, Ts..., uint64_t>::call(args);
+
+          // floating point gets promoted to double but a different path
+        case func_piecewise_pass_element_double:
+          return interpT<ToDerive, NextDerived, Ts..., double>::call(args);
+
+          // return interpT<ToDerive, NextDerived, Ts..., void*>{}(args);
+          // return interpT<ToDerive, NextDerived, Ts..., const char*>{}(args);
         default:
           printf("error: unhandled enum\n");
       }
@@ -164,43 +226,32 @@ template <size_t ToDerive, typename... Ts>
 struct interpT<ToDerive, ToDerive, Ts...>
 {
   using asTup = std::tuple<Ts...>;
-
   static_assert(sizeof...(Ts) == ToDerive, "");
-  void operator()(const std::vector<field>& args)
+  static void call(const std::vector<field>& args)
   {
-    printf("Narg %zu (", ToDerive);
-    if constexpr (ToDerive > 0)
-      {
-        dump<typename std::tuple_element<0, asTup>::type>{};
-      }
-    if constexpr (ToDerive > 1)
-      {
-        dump<typename std::tuple_element<1, asTup>::type>{};
-      }
-    if constexpr (ToDerive > 2)
-      {
-        dump<typename std::tuple_element<2, asTup>::type>{};
-      }
-    if constexpr (ToDerive > 3)
-      {
-        dump<typename std::tuple_element<3, asTup>::type>{};
-      }
-    if constexpr (ToDerive > 4)
-      {
-        dump<typename std::tuple_element<4, asTup>::type>{};
-      }
-    printf(")\n");
+    baseprint<ToDerive, asTup>(args);
   }
 };
 
 template <size_t N>
 void interpN(const std::vector<field>& args)
 {
-  interpT<N, 0> inst;
-  inst(args);
+  interpT<N, 0>::call(args);
 }
 
-void interp(const std::vector<field>& args)
+}  // namespace
+
+extern "C" void interp_2(const std::vector<field>& args)
+{
+  return interpN<2>(args);
+}
+
+extern "C" void interp_3(const std::vector<field>& args)
+{
+  return interpN<3>(args);
+}
+
+extern "C" void interp(const std::vector<field>& args)
 {
   switch (args.size())
     {
@@ -216,11 +267,11 @@ void interp(const std::vector<field>& args)
         return interpN<4>(args);
       case 5:
         return interpN<5>(args);
-#if 0
       case 6:
         return interpN<6>(args);
       case 7:
         return interpN<7>(args);
+#if 0
       case 8:
         return interpN<8>(args);
       case 9:
