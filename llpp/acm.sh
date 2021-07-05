e206fe868e1652c3fa43719642996dfb4b9dea5d
Jon Chesterfield <jonathanchesterfield@gmail.com>
2021-07-05 19:16:20 +0100

    splice a bunch of templates into the zip file, downgrade a latex package to work around acm error

--- llpp/acm.sh
+++ llpp/acm.sh
@@ -16,6 +16,21 @@ cp ../../reference.bib Source/
 
 mkdir supplements
 
+# Maybe it needs this stuff in the source directory too?
+# Doesn't have a license on it so assuming I can't paste it into the repo
+wget https://www.acm.org/binaries/content/assets/publications/consolidated-tex-template/acmart-primary.zip
+
+# Delete stuff to try to get back below their 10mb limit
+unzip acmart-primary.zip -d .
+rm -rf acmart-primary.zip acmart-primary/samples acmart-primary/.gitignore
+mv acmart-primary/* Source/
+rmdir acmart-primary
+
+rm -rf acmart-primary.zip Source/acmart-primary/samples
+
 cd -
 
 zip -r $NAME.zip $NAME
+
+du -sm $NAME.zip
+
